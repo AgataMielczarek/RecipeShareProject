72dd33114a95d7c9208fd4c5dd9ecde0427ce69c
Agata <amielczarek09@gmail.com>
2021-08-02 22:21:52 +0200

    Model Recipe - create & register

--- rec/models.py
+++ rec/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=250)
+    time = models.CharField(max_length=8)
+    servings = models.CharField(max_length=3)
+    ingridients = models.TextField()
+    desc = models.TextField()
+
+    cuisines = [
+        ('', 'Choose one'),
+        ('tr', 'Traditional'),
+        ('vt', 'Vegetarian'),
+        ('ve', 'Vegan'),
+    ]
+    cuisine = models.CharField(max_length=2, choices=cuisines, default='')
+
+    levels = [
+        ('', 'Choose one'),
+        ('e', 'Easy'),
+        ('m', 'Medium'),
+        ('h', 'Hard'),
+    ]
+    level = models.CharField(max_length=1, choices=levels, default='')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # favourites
+    # pictures
+    # comments
+    
+    
+
+    def __str__(self):
+        return self.name
+
+
