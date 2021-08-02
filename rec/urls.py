7c8768dda45508e76d84ab00ef05eb7b39458828
Agata <amielczarek09@gmail.com>
2021-08-02 22:12:28 +0200

    Login & logout

--- rec/urls.py
+++ rec/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('register', views.register, name='register'),
+    path('login', views.log, name='log'),
+    path('logout', views.logoutuser, name='logout'),
 ]
--- rec/views.py
+++ rec/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from django.contrib.auth import authenticate, login, logout
 
 def register(request):
     if request.method == 'GET':
@@ -16,11 +17,29 @@ def register(request):
                 return render(request, 'register.html', {'form':UserCreationForm(), 'error': error})
             else:
                 user.save()
-                # login user
+                login(request, user)
                 return redirect('home')
         else:
             error = 'Password did not match. Try again.'
             return render(request, 'register.html', {'form': UserCreationForm, 'error': error})
+
+def log(request):
+    if request.method == 'GET':
+        return render(request, 'log.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(username=request.POST.get(
+            'username'), password=request.POST.get('password'))
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            error = 'Username or password is wrong. Try again.'
+            return render(request, 'log.html', {'form': AuthenticationForm(), 'error': error})
+
+def logoutuser(request):
+    logout(request)
+    return redirect('home')
+
 
 def home(request):
     return render(request, 'home.html')
